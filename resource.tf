1614e6146b7f7988c6b51fb2a93de10fa2ee3431
krupasandhya <nandivadasanju2@gmail.com>
2025-07-17 18:16:11 +0000

    blob

--- resource.tf
+++ resource.tf
@@ -9,3 +9,8 @@ resource "azurerm_storage_account" "storyterra" {
     account_tier             = "Standard"
     account_replication_type = "LRS"
 }
+resource "azurerm_storage_container" "storycontainer" {
+  name                  = "nksblobcontainer"
+  storage_account_name  = azurerm_storage_account.storyterra.name
+  container_access_type = "private"
+}
