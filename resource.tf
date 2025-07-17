83bfd3c1b399555c3beeed1c43c6feabb604f7c6
krupasandhya <nandivadasanju2@gmail.com>
2025-07-17 11:23:48 +0000

    sa

--- resource.tf
+++ resource.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "terraflow" {
   name     = "terraflow"
   location = "East US"
 }
+resource "azurerm_storage_account" "storageterra" {
+    name                     = "sandystorageterra987"
+    resource_group_name      = azurerm_resource_group.terraflow.name
+    location                 = azurerm_resource_group.terraflow.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
